19a4aac5eb8786f4137d78f17217c8e2810eb33c
Gozal12107 <galizada12107@ada.edu.az>
2024-01-03 22:07:33 +0400

    recreating, adding fliptoggle, onclick answer change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState(PROTOTYPE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlashcardList flashcards={flashcards} />
   );
 }
 
+const PROTOTYPE_FLASHCARDS = [
+{
+  id: 1,
+  question: 'Which president is Lincoln of USA?',
+  answer: '16th',
+  options: [
+    '23rd',
+    '32nd',
+    '1st',
+    '8th'
+  ]
+},
+{
+  id: 2,
+  question: 'Question 2?',
+  answer: 'Answer',
+  options: [
+    'Answer',
+    'Answer 1',
+    'Answer 2',
+    'Answer 3'
+  ]
+}
+]
 export default App;
